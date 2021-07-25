6f15749f8d5d9d7e77a4624f4548fdd31bff2ab7
abDevII <barbey.arthur@gmail.com>
2021-07-25 16:29:03 +0200

    Gestion du rendu de l'UI lors du fetch de la liste des livres

--- src/components/BookstoreComponent.js
+++ src/components/BookstoreComponent.js
@@ -1,8 +1,60 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { Component } from "react";
 
-function Bookstore(props) {
-  return <div className="container"></div>;
+class Bookstore extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      error: false,
+      areBooksLoaded: false,
+      books: [],
+    };
+
+    this.loadBooks = this.loadBooks.bind(this);
+  }
+
+  loadBooks() {
+    fetch("https://henri-potier.techx.fr/books")
+      .then((res) => res.json())
+      .then(
+        (result) => {
+          this.setState({
+            areBooksLoaded: true,
+            books: result
+          });
+        },
+        (error) => {
+          this.setState({
+            areBooksLoaded: true,
+            error: true
+          });
+        }
+      );
+  }
+
+  componentDidMount() {
+    // Get the books data as soon as everything else have been loaded
+    this.loadBooks();
+  }
+
+  render() {
+    const { error, areBooksLoaded, books } = this.state;
+    if (error) {
+      return <div>Un problème est survenu et nous en sommes désolés, veuillez bien s'il vous plaît rafraîchir la page ou revenir plus tard</div>;
+    } else if (!areBooksLoaded) {
+      return <div>Chargement…</div>;
+    } else {
+      return (
+        <ul>
+          {books.map((book) => (
+            <li key={book.title}>
+              {book.title} {book.price}
+            </li>
+          ))}
+        </ul>
+      );
+    }
+  }
 }
 
 export default Bookstore;
